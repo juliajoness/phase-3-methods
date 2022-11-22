1172248f6d3744e9bb833f8eb0bc8d53eb071992
julia <juliajaj3@gmail.com>
2022-11-22 11:17:06 -0500

    commit tree 13733f22781c4990f45ef8efa27da4909b0e4ebe

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Rubyist")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+  puts "Hello, Naureen!"
+  puts "Hello, programmer!"
+
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(x)
+    x.is_a?(Integer) ? x/ 2 : nil
+  end
